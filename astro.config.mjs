3dd3eb9b1e191453f4ea5820abcfa2a3eebf641a
izakikazuho <izaki@mud.co.jp>
2025-10-08 13:00:39 +0900

    add dialog test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
+/** @format */
+
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [],
+  image: {
+    remotePatterns: [],
+  },
+  vite: {
+    css: {},
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  },
+});
